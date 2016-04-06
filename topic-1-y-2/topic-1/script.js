84e4b20606b15de7963ddcbcbc0a92fe7147582f
Bocha <Bocha>
2016-04-06 10:26:42 -0300

    execises 1,2,3,4,5,6

---
+++ topic-1-y-2/topic-1/script.js
@@ -0,0 +1,23 @@
+function showSection(){
+	document.getElementById("hidden").style.visibility = "visible";
+}
+function returnFunction(){
+	var req = new XMLHttpRequest();
+
+	req.onreadystatechange = showContain;
+	req.open('GET', 'http://api.icndb.com/jokes/random', true);
+	req.send(null)
+
+
+	function showContain(){
+	if(req.readyState ==4){
+		if(req.status==200){
+			document.write(req.responseText);
+		}else if (req.status==404){
+			document.write("404 NOT FOUND");
+		}else{
+			document.write("500 INTERNAL SERVER ERROR")
+		}
+	}
+	}
+}
