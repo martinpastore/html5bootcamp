5b274dd4db5f98290f90a412ad4eba09e6acba48
Bocha <Bocha>
2016-04-06 12:08:06 -0300

    work in progress. exercise 7

--- topic-1-y-2/topic-1/script.js
+++ topic-1-y-2/topic-1/script.js
@@ -16,8 +16,33 @@ function returnFunction(){
 		}else if (req.status==404){
 			document.write("404 NOT FOUND");
 		}else{
-			document.write("500 INTERNAL SERVER ERROR")
+			document.write("500 INTERNAL SERVER ERROR");
 		}
 	}
 	}
 }
+/*
+	function ajaxCall(){
+		var req = new XMLHttpRequest();
+		var url = document.getElementById("uri").value;
+
+	req.onreadystatechange = showAjax;
+	$.ajax({
+		url: url;
+		type: "GET"
+		async: false;
+		});
+
+	function showAjax(){
+	if(req.readyState==4){
+		if(req.status==200){
+			document.write(req.responseText);
+		}else if (req.status==404){
+			document.write("404 NOT FOUND");
+		}else{
+			document.write("500 INTERNAL SERVER ERROR");
+		}
+	}
+	}
+}
+*/
