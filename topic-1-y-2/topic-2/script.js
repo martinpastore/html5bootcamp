f12c5cc6189b2c8e3a5f63b285dab604d95fb963
Bocha <Bocha>
2016-04-08 09:50:37 -0300

    start topic 2

---
+++ topic-1-y-2/topic-2/script.js
@@ -0,0 +1,21 @@
+var Movie = (function(){
+	this.title;
+	this.year;
+	this.duration;
+});
+
+Movie.prototype.play = function(){
+
+}
+Movie.prototype.pause = function(){
+
+}
+Movie.prototype.resume = function(){
+
+}
+
+m = new Movie();
+m.title = "Harry Potter";
+
+n = new Movie();
+n.title = "Star Wars"
