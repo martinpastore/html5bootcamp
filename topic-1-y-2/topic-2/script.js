84793f9cea3e5134a2a566600a49c288a02b460c
Bocha <Bocha>
2016-04-09 10:37:58 -0300

    exercise 3 topic 2

--- topic-1-y-2/topic-2/script.js
+++ topic-1-y-2/topic-2/script.js
@@ -14,14 +14,42 @@ Movie.prototype.resume = function(){
 
 };
 var EventEmmitter = (function(){
-
+/*fijense en la doc de trigger() on() live() off() y vean que basicamente les estan pidiendo que enmascaren esas funciones*/	
 });
-EventEmmitter.prototype.on = function(){
-	
-};
-EventEmmitter.prototype.emit = function(){
 
-};
-EventEmmitter.prototype.off = function(){
+EventEmmitter.prototype.on = $(document).ready(function(){
+	$("#btnMovie1").on("click", function(){
+		$("#sw7").removeClass("hide");
+		$("#hp7").addClass("hide");
+		$("#im3").addClass("hide");
+	});
+	$("#btnMovie2").on("click", function(){
+		$("#sw7").addClass("hide");
+		$("#hp7").removeClass("hide");
+		$("#im3").addClass("hide");
+	});
+	$("#btnMovie3").on("click", function(){
+		$("#sw7").addClass("hide");
+		$("#hp7").addClass("hide");
+		$("#im3").removeClass("hide");
+	});
+	/*esta funcion lo que hace es enmascarar un listener (vean que es un listener)
+	lo que va a hacer es tener un callback que hace algo cuando un evento X se trigguerea*/
+});
+EventEmmitter.prototype.emit = $(document).ready(function(){
+$("#showMov").click(function(){	
+	$("input").trigger("select");
+	});
+$("input").select(function(){
+	$("#cart").removeClass("hide");
+	});
+/*2) emit es la funcion que triggerea los eventos*/
+});
+EventEmmitter.prototype.off = $(document).ready(function(){
+	$("sw7").off("click")
+	$("hp7").off("click")
+	$("im3").off("click")
+/*off borra todos los listeners de los eventos que le pasas*/
+});
 
-};
+
