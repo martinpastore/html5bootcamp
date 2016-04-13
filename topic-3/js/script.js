61721c66f44d97f9fcc22f250f03be69133b4f8b
Bocha <Bocha>
2016-04-13 14:06:41 -0300

    changes exercise 2

--- topic-3/js/script.js
+++ topic-3/js/script.js
@@ -1,8 +1,8 @@
 var moviesApp = angular.module('moviesApp', ["ngRoute"]);
-/*
-moviesApp.controller('MainCtrl', ['$scope', function($scope){
-	/*$scope.movie1 = {};
-	$scope.movie1.details = {
+
+moviesApp.controller('MoviesCtrl', ['$scope', function($scope){
+	$scope.movie = {};
+	$scope.movie.details = [{
 		"name": "The Avengers",
 		"actor1": "Robert Downey Jr.",
 		"actor2": "Chris Evans",
@@ -10,41 +10,33 @@ moviesApp.controller('MainCtrl', ['$scope', function($scope){
 		"actor4": "Mark Ruffalo",
 		"actor5": "Chris Hemsworth",
 		"premiere": "26 de abril de 2012"
-	};
-	$scope.movie2 = {};
-	$scope.movie2.details = {
+	},{
 		"name": "Star Wars 7",
 		"actor1": "Mark Hamill",
 		"actor2": "Adam Driver",
 		"actor3": "Harrison Ford",
 		"premiere": "17 de diciembre de 2015"
-	};
-	$scope.movie3 = {};
-	$scope.movie3.details = {
+	},{
 		"name": "Harry Potter 7",
 		"actor1": "Daniel Radcliffe",
 		"actor2": "Emma Watson",
 		"actor3": "Rupert Grint",
 		"premiere": "7 de Julio de 2011"
-	};
-	$scope.movie4 = {};
-	$scope.movie4.details = {
+	},{
 		"name": "Fast and Furious 7",
 		"actor1": "Paul Walker",
 		"actor2": "Vin Diesel",
 		"actor3": "Dwayne Johnson",
 		"premiere": "2 de abril de 2015"
-	};
-	$scope.movie5 = {};
-	$scope.movie5.details = {
+	},{
 		"name": "The Hunger Games",
 		"actor1": "Jennifer Lawrence",
 		"actor2": "Josh Hutcherson",
 		"actor3": "Liam Hemsworth",
 		"premiere": "4 de noviembre de 2015"
-	};
+	}];
 }]);
-}*/
+
 
 moviesApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 			$scope.Movies = {};
@@ -53,20 +45,36 @@ moviesApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 
 			$http({
 				method: 'GET',
-				url: 'js/mov.json'
+				url: ''
 
 			})
 			.success(function(data){
-				$scope.Movies.name = data.Movies.name;
-				$scope.Movies.actor1 = data.Movies.actor1;
-				$scope.Movies.actor2 = data.Movies.actor2;
-				$scope.Movies.actor3 = data.Movies.actor3;
-				$scope.Movies.premiere = data.Movies.premiere;
+				$scope.movie.name = data.movie.name;
+				$scope.movie.actor1 = data.movie.actor1;
+				$scope.movie.actor2 = data.movie.actor2;
+				$scope.movie.actor3 = data.movie.actor3;
+				$scope.movie.premiere = data.movie.premiere;
 			});	
 	}]);
-	//boton
-	/*moviesApp.directive('customButton', function(){
-		return{
 
-		}
-	});*/
+moviesApp.config('MainCtrl', ['$routeProvider', function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: "view/avengers.html"
+	})
+	.when('/', {
+		templateUrl: "view/sw7.html"
+	})
+	.when('/', {
+		templateUrl: "view/hp7.html"
+	})
+	.when('/', {
+		templateUrl: "view/faf7.html"
+	})
+	.when('/', {
+		templateUrl: "view/hunger.html"
+	})
+	.otherwirse({
+		redirectTo: '/'
+	});
+}]);
