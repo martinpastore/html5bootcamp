4e253b6d46436a3b98514a79569782c8f315050e
Bocha <Bocha>
2016-04-13 14:30:28 -0300

    changes exercise 2

--- topic-3/js/script.js
+++ topic-3/js/script.js
@@ -2,41 +2,59 @@ var moviesApp = angular.module('moviesApp', ["ngRoute"]);
 
 moviesApp.controller('MoviesCtrl', ['$scope', function($scope){
 	$scope.movie = {};
-	$scope.movie.details = [{
-		"name": "The Avengers",
+	$scope.movie.name = [{
+		"name": "The Avengers"
+	},{
+		"name": "Star Wars 7"
+	},{
+		"name": "Harry Potter 7"
+	},{
+		"name": "Fast and Furious 7"
+	},{
+		"name": "The Hunger Games"
+	}];
+}]);
+
+moviesApp.controller('AvenCtrl', ['$scope' ,'$http', function($scope, $html){
+	$scope.details= {
 		"actor1": "Robert Downey Jr.",
 		"actor2": "Chris Evans",
 		"actor3": "Scarlett Johansson",
-		"actor4": "Mark Ruffalo",
-		"actor5": "Chris Hemsworth",
 		"premiere": "26 de abril de 2012"
-	},{
-		"name": "Star Wars 7",
+	};
+}]);
+moviesApp.controller('StarCtrl', ['$scope' ,'$http', function($scope, $html){
+	$scope.details= {
 		"actor1": "Mark Hamill",
 		"actor2": "Adam Driver",
 		"actor3": "Harrison Ford",
 		"premiere": "17 de diciembre de 2015"
-	},{
-		"name": "Harry Potter 7",
+	};
+}]);
+moviesApp.controller('HarryCtrl', ['$scope' ,'$http', function($scope, $html){
+	$scope.details= {
 		"actor1": "Daniel Radcliffe",
 		"actor2": "Emma Watson",
 		"actor3": "Rupert Grint",
 		"premiere": "7 de Julio de 2011"
-	},{
-		"name": "Fast and Furious 7",
+	};
+}]);
+moviesApp.controller('FastCtrl', ['$scope' ,'$http', function($scope, $html){
+	$scope.details= {
 		"actor1": "Paul Walker",
 		"actor2": "Vin Diesel",
 		"actor3": "Dwayne Johnson",
 		"premiere": "2 de abril de 2015"
-	},{
-		"name": "The Hunger Games",
+	};
+}]);
+moviesApp.controller('HungerCtrl', ['$scope' ,'$http', function($scope, $html){
+	$scope.details= {
 		"actor1": "Jennifer Lawrence",
 		"actor2": "Josh Hutcherson",
 		"actor3": "Liam Hemsworth",
 		"premiere": "4 de noviembre de 2015"
-	}];
+	};
 }]);
-
 
 moviesApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 			$scope.Movies = {};
@@ -57,22 +75,27 @@ moviesApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 			});	
 	}]);
 
-moviesApp.config('MainCtrl', ['$routeProvider', function($routeProvider){
+moviesApp.config('MainCtrl', ['ngRoute', function($routeProvider){
 	$routeProvider
 	.when('/', {
-		templateUrl: "view/avengers.html"
+		templateUrl: "view/avengers.html",
+		controller: 'AvenCtrl' 
 	})
 	.when('/', {
-		templateUrl: "view/sw7.html"
+		templateUrl: "view/sw7.html",
+		controller: 'StarCtrl'
 	})
 	.when('/', {
-		templateUrl: "view/hp7.html"
+		templateUrl: "view/hp7.html",
+		controller: 'HarryCtrl'
 	})
 	.when('/', {
-		templateUrl: "view/faf7.html"
+		templateUrl: "view/faf7.html",
+		controller: 'FastCtrl'
 	})
 	.when('/', {
-		templateUrl: "view/hunger.html"
+		templateUrl: "view/hunger.html",
+		controller: 'HungerCtrl'
 	})
 	.otherwirse({
 		redirectTo: '/'
