095d09bfde19c6691df54107962de3fa8dce382e
Bocha <Bocha>
2016-04-13 10:27:01 -0300

    exercise 2 topic 3

--- topic-3/js/script.js
+++ topic-3/js/script.js
@@ -4,22 +4,43 @@ moviesApp.controller('MainCtrl', ['$scope', function($scope){
 	$scope.movie1 = {};
 	$scope.movie1.details = {
 		"name": "The Avengers",
-		"actors": "Bob Marley xD"
+		"actor1": "Robert Downey Jr.",
+		"actor2": "Chris Evans",
+		"actor3": "Scarlett Johansson",
+		"actor4": "Mark Ruffalo",
+		"actor5": "Chris Hemsworth",
+		"premiere": "26 de abril de 2012"
 	};
 	$scope.movie2 = {};
 	$scope.movie2.details = {
-
+		"name": "Star Wars 7",
+		"actor1": "Mark Hamill",
+		"actor2": "Adam Driver",
+		"actor3": "Harrison Ford",
+		"premiere": "17 de diciembre de 2015"
 	};
 	$scope.movie3 = {};
 	$scope.movie3.details = {
-
+		"name": "Harry Potter 7",
+		"actor1": "Daniel Radcliffe",
+		"actor2": "Emma Watson",
+		"actor3": "Rupert Grint",
+		"premiere": "7 de Julio de 2011"
 	};
 	$scope.movie4 = {};
 	$scope.movie4.details = {
-
+		"name": "Fast and Furious 7",
+		"actor1": "Paul Walker",
+		"actor2": "Vin Diesel",
+		"actor3": "Dwayne Johnson",
+		"premiere": "2 de abril de 2015"
 	};
 	$scope.movie5 = {};
 	$scope.movie5.details = {
-
+		"name": "The Hunger Games",
+		"actor1": "Jennifer Lawrence",
+		"actor2": "Josh Hutcherson",
+		"actor3": "Liam Hemsworth",
+		"premiere": "4 de noviembre de 2015"
 	};
 }]);
