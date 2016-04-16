96df804b644917e8f8ba611c6e5bc3f7e2f2b17c
Bocha <Bocha>
2016-04-16 13:07:27 -0300

    final test

--- topic-3/test/test.js
+++ topic-3/test/test.js
@@ -1,18 +1,12 @@
 describe('test', function(){
 	beforeEach(module('controller'));
 
-		var movies;
+		var index;
 
-	beforeEach(inject(function(_movies_details_){
-		movies = _movies_details_;
+	beforeEach(inject(function($index){
+		index = $index;
 	}));
-	it('empty movies length 0', function(){
-		expect(movies.all().length).toBe(0);
-	});
-
-	it('One movies return length 1', function(){
-		movies.add({name: 'movies.details name'});
-
-		expect(movies.all().length).toBe(1);
-	});
+	if(index < 0){
+		return;
+	};
 });
